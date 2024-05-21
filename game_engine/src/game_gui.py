038d26afa707489ea3d0e3aacf6ee07dc6ed64e7
JanetRivera <clarajanet16180@outlook.com>
2024-05-21 17:03:03 -0600

    Update gui

--- game_engine/src/game_gui.py
+++ game_engine/src/game_gui.py
@@ -1,6 +1,7 @@
 import pygame
 from pathlib import Path
-from .settings import SCREEN_WIDTH, SCREEN_HEIGHT
+from .settings import SCREEN_WIDTH, SCREEN_HEIGHT, NUMBER_PLAYERS
+from .game_logic import Game_logic, Game_viewer
 import math
 
 ROOT = Path(__file__).parent
@@ -8,13 +9,28 @@ ROOT = Path(__file__).parent
 class Game_GUI():
     def __init__(self, screen):
         self.screen = screen
+
+        self.game_logic = Game_logic(number_players=NUMBER_PLAYERS)
+        # self.game_logic.fill_factories()
+
+        self.game_viewer = Game_viewer(self.game_logic)
+
+
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.factories_num = 9
         self.factories_positions = []
 
         self.__init_screen_setup()
+
+        self.tiles_colors_dict = {
+            1: (87,161,196),
+            2: (3,3,2),
+            3: (235,65,74),
+            4: (99,209,223),
+            5: (238,194,114),
+        }
+
         self.draw_tiles()
 
     def __init_screen_setup(self):
@@ -22,12 +38,11 @@ class Game_GUI():
         board_image = pygame.image.load(board_image_path)
         botton_position = SCREEN_HEIGHT - board_image.get_height()
 
-        # Draw players boards
         self.screen.fill((255, 255, 255)) 
-        self.screen.blit(board_image, (0, botton_position))
-        self.screen.blit(board_image, (board_image.get_width(), botton_position))
-        self.screen.blit(board_image, (board_image.get_width()*2, botton_position))
-        self.screen.blit(board_image, (board_image.get_width()*3, botton_position))
+
+        # Draw players boards
+        for i in range(self.game_viewer.get_number_of_players()):
+            self.screen.blit(board_image, (board_image.get_width()*i, botton_position))
 
         # Draw factories circles
         circle_color = (238,229,222)
@@ -38,8 +53,9 @@ class Game_GUI():
         cx = SCREEN_WIDTH//2
         cy = radius_big_circle + circle_radius + 10
 
-        for i in range(self.factories_num):
-            angle = 2 * math.pi * i / self.factories_num + 3*math.pi/2
+        factories_num = len(self.game_viewer.get_factories())
+        for i in range(factories_num):
+            angle = 2 * math.pi * i / factories_num + 3*math.pi/2
             x = int(cx + radius_big_circle * math.cos(angle))
             y = int(cy + radius_big_circle * math.sin(angle))
             self.factories_positions.append((x,y))
@@ -49,7 +65,12 @@ class Game_GUI():
     def draw_tiles(self):
 
         tile_color = (87,161,196)
-        for factory_position in self.factories_positions:
+
+        factories = self.game_viewer.get_factories
+        for i in range(len(factories)):
+            factory_tiles = factories[i]
+            factory_position = self.factories_positions[i]
+            
             tile_size = 40
             separation = 5
 
@@ -77,11 +98,13 @@ class Game_GUI():
                 tile_size,
                 tile_size
             )
+            
+            tile_colors =  [self.tiles_colors_dict[tile_type] for tile_type in factory_tiles]
 
-            pygame.draw.rect(self.screen, tile_color, tile_1)
-            pygame.draw.rect(self.screen, tile_color, tile_2)
-            pygame.draw.rect(self.screen, tile_color, tile_3)
-            pygame.draw.rect(self.screen, tile_color, tile_4)
+            pygame.draw.rect(self.screen, tile_colors[0], tile_1)
+            pygame.draw.rect(self.screen, tile_colors[1], tile_2)
+            pygame.draw.rect(self.screen, tile_colors[2], tile_3)
+            pygame.draw.rect(self.screen, tile_colors[3], tile_4)
 
        
 
